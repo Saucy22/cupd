5691463d6414c337eb2fa8c572c2c702074c4539
Saucy22 <124105644+Saucy22@users.noreply.github.com>
2025-06-26 12:54:51 -0400

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,4 +1,5 @@
 echo Copying files..
 sudo cp Cuppy /bin -r
+sudo chmod a+x cupd
 sudo cp cupd /bin
-echo Done! You can now run Caleb's Updater in the terminal by using 'cupd'
+echo Done! You can now run Calebs Updater in the terminal by using cupd
